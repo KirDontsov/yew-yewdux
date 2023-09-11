c4cc3e9dbf68440b620c3071b5bd0db7940e3971
Кирилл Донцов <KDontsov@integrity-solutions.ru>
2023-09-12 01:03:23 +0300

    add copy text to clipboard

--- src/components/table/table_cell.rs
+++ src/components/table/table_cell.rs
@@ -1,4 +1,5 @@
 use yew::prelude::*;
+use yew_hooks::prelude::*;
 
 #[derive(Debug, PartialEq, Properties)]
 pub struct Props {
@@ -7,8 +8,18 @@ pub struct Props {
 
 #[function_component(TableCell)]
 pub fn table_cell(props: &Props) -> Html {
+	let clipboard = use_clipboard();
+
+    let onclick_write_text = {
+      let clipboard = clipboard.clone();
+	    let clonned_content = props.content.clone();
+      Callback::from(move |_| {
+        clipboard.write_text(clonned_content.clone());
+      })
+    };
+
 	html!(
-		<td scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+		<td onclick={onclick_write_text} scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
 				{props.content.clone()}
 	  </td>
 	)
